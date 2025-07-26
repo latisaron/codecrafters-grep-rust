85468140f794cc328637e0171718665d151b872a
aron <latis.aron@gmail.com>
2025-07-26 08:41:56 -0400

    pattern mathcing

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
+    } else if pattern == "\\d" {
+        return input_line.chars().any(|c| matches!(c, '0'..='9'));
     } else {
-        panic!("Unhandled pattern: {}", pattern)
+        panic!("Unhandled pattern: {}", pattern);
     }
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+    eprintln!("Logs from your program will appear here!!!");
 
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
